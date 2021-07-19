88cd3b4d7107bf72d3077df688e5190a4009a164
jrphan <73537516+jrphan@users.noreply.github.com>
2021-07-19 21:33:59 +0700

    form

--- src/pages/Contactpage.js
+++ src/pages/Contactpage.js
@@ -12,22 +12,25 @@ function Contactpage() {
                     <div className="left-content">
                         <form className="form">
                             <div className="form-group">
+                                <input type="text" id="name" placeholder=" "/>
                                 <label htmlFor="name">Enter your name:</label>
-                                <input type="text" id="name"/>
                             </div>
                             <div className="form-group">
+                                <input type="email" id="email" placeholder=" "/>
                                 <label htmlFor="email">Enter your email:</label>
-                                <input type="email" id="email"/>
                             </div>
                             <div className="form-group">
+                                <input type="text" id="subject" placeholder=" "/>
                                 <label htmlFor="subject">Enter your subject:</label>
-                                <input type="text" id="subject"/>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="email">Enter your email:</label>
-                                
+                                <textarea id="text-area" name="textarea" cols="48" rows="5" placeholder=" "></textarea>
+                                <label htmlFor="text-area">Enter your Message:</label>
                             </div>
                         </form>
+                    </div>
+                    <div className="right-content">
+                        hello
                     </div>
                </InnerLayout>
            </ContactStyled>
@@ -36,7 +39,83 @@ function Contactpage() {
 }
 
 const ContactStyled = styled.section `
+    .contact-section {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
 
+        .form {
+            width: 100%;
+
+            &-group {
+                margin-top: 1.5rem;
+                position: relative;
+                width: 100%;
+
+                label {
+                    position: absolute;
+                    padding: 0 5px;
+                    border-radius: 5px;
+                    top: 50%;
+                    transform: translateY(-50%);
+                    left: 20px;
+                  
+                    user-select: none;
+                    transition: all .3s ease;
+                    opacity: 0.5;
+                }
+
+                input {
+                    border: 1px solid var(--border-color);
+                    outline: none;
+                    background: transparent;
+                    color: var(--white-color);
+                    height: 50px;
+                    padding: 5px 15px;
+                    width: 100%;
+                    border-radius: 15px;
+
+                    &:focus {
+                        border: 1px solid var(--primary-color);
+                    }
+
+                    &:not(:placeholder-shown) + label, &:focus + label {
+                        left: 20px;
+                        top: -12px;
+                        display: inline-block;
+                        background-color: var(--background-dark-color);
+                        transform: none;
+                        transition: all .3s ease;
+                        opacity: 1;
+                        color: var(--white-color);
+                    }
+                }
+
+                textarea {
+                    border: 1px solid var(--border-color);
+                    outline: none;
+                    background: transparent;
+                    color: var(--white-color);
+                    padding: 15px;
+                    border-radius: 15px;
+                    resize: none;
+                    &:focus {
+                        border: 1px solid var(--primary-color);
+                    }
+
+                    &:not(:placeholder-shown) + label, &:focus + label {
+                        left: 20px;
+                        top: -12px;
+                        display: inline-block;
+                        background-color: var(--background-dark-color);
+                        transform: none;
+                        transition: all .3s ease;
+                        opacity: 1;
+                        color: var(--white-color);
+                    }
+                }
+            }
+        }
+    }
 `;
 
 export default Contactpage
