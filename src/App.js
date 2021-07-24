77e96995e38ce79422463bee76fb15e6b4c279c8
jrphan <73537516+jrphan@users.noreply.github.com>
2021-07-24 19:27:59 +0700

    fix error

--- src/App.js
+++ src/App.js
@@ -2,33 +2,48 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar";
 import React, { Suspense } from "react";
-// import Loading from "./components/Loading";
+import Homepage from "./pages/Homepage";
+import Aboutpage from "./pages/Aboutpage";
+import Resumepage from "./pages/Resumepage";
+import Portfoliopage from "./pages/Portfoliopage";
+import Contactpage from "./pages/Contactpage";
 
-const Home = React.lazy(() => import('./pages/Homepage'));
-const About = React.lazy(() => import('./pages/Aboutpage'));
-const Resume = React.lazy(() => import('./pages/Resumepage'));
-const Portfolios = React.lazy(() => import('./pages/Portfoliopage'));
-const Contact = React.lazy(() => import('./pages/Contactpage'));
+
+// const Home = React.lazy(() => import('./pages/Homepage'));
+// const About = React.lazy(() => import('./pages/Aboutpage'));
+// const Resume = React.lazy(() => import('./pages/Resumepage'));
+// const Portfolios = React.lazy(() => import('./pages/Portfoliopage'));
+// const Contact = React.lazy(() => import('./pages/Contactpage'));
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading....</div>}>
+      {/* <Suspense fallback={<div>Loading....</div>}> */}
         <BrowserRouter>
           <Sidebar/>
           <MainContentStyled>
               <Switch>
                 <Redirect exact from="/" to="/home"/>
 
-                <Route path="/home" exact component={Home}/>
-                <Route path="/about" exact component={About}/>
-                <Route path="/resume" exact component={Resume}/>
-                <Route path="/portfolios" exact component={Portfolios}/>
-                <Route path="/contact" exact component={Contact}/>
+                <Route path="/home" exact>
+                  <Homepage/>
+                </Route> 
+                <Route path="/about" exact>
+                  <Aboutpage/>
+                </Route>
+                <Route path="/resume" exact>
+                  <Resumepage/>
+                </Route>
+                <Route path="/portfolios" exact>
+                  <Portfoliopage/>
+                </Route>
+                <Route path="/contact" exact>
+                  <Contactpage/>
+                </Route>
               </Switch>
           </MainContentStyled>
         </BrowserRouter>
-      </Suspense>
+      {/* </Suspense> */}
     </div>
   );
 }
